d2cd343cb722effa9d58fb0b91d27d0d302e4575
Vitor11001 <josevitorleao1@gmail.com>
2024-08-11 19:17:33 -0300

    .

--- scripts/install_bspwm.sh
+++ scripts/install_bspwm.sh
@@ -11,13 +11,22 @@ sudo pacman -S --needed \
     xorg-xrandr \       # Utilitário para configurar a resolução da tela.
     xorg-xsetroot \     # Utilitário para definir a cor da raiz.
     xorg-xprop \        # Utilitário para visualizar propriedades de janelas.
-    xorg-xev
+    xorg-xev            # Utilitário para visualizar eventos do teclado e mouse.
 
 # Instalando bspwm
 echo "Instalando bspwm e cia..."
 sudo pacman -S --needed \
     bspwm \
     sxhkd 
+
+# Instalando fontes
+echo "Instalando fontes..."
+sudo pacman -S --needed \
+    ttf-firecode \
+    ttf-font-awesome \
+    noto-fonts-emoji \
+    ttf-meslo-nerd-font \
+    ttf-roboto
 
 # Instalando programas adicionais
 echo "Instalando programas adicionais..."
@@ -29,4 +38,5 @@ sudo pacman -S --needed \
     xdg-user-dirs-gtk \
     pavucontrol \
     openssh \
-    thunar
+    thunar \
+    nitrogen
