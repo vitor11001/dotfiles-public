08d8b609f254018d90cba54166e29a87e4bf311e
Vitor11001 <josevitorleao1@gmail.com>
2024-08-11 01:51:03 -0300

    modificando os pacotes adicionais

--- scripts/install_bspwm.sh
+++ scripts/install_bspwm.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
+echo "Atualizando o sistema..."
+sudo pacman -Syu
+
 # Instalando dependências
 echo "Instalando pacotes necessários..."
 sudo pacman -S --needed \
-    xorg \
-    xorg-xinit \
-    xclip \
-    libxcb \
-    xcb-utils-keysyms \
-    xcb-util-cursor
+    xorg-server \       # O servidor X, necessário para qualquer ambiente gráfico.
+    xorg-xinit \        # Inicializador do servidor X.
+    xorg-xrandr \       # Utilitário para configurar a resolução da tela.
+    xorg-xsetroot \     # Utilitário para definir a cor da raiz.
+    xorg-xprop          # Utilitário para visualizar propriedades de janelas.
 
 # Instalando bspwm
 echo "Instalando bspwm e cia..."
 sudo pacman -S --needed \
     bspwm \
-    sxhkd \
-    alacritty \
+    sxhkd 
+
+# Instalando programas adicionais
+echo "Instalando programas adicionais..."
+sudo pacman -S --needed \
+    kitty \
+    firefox \
     rofi \
-    polybar \
     vim \
     xdg-user-dirs-gtk \
+    pavucontrol \
+    openssh \
+    thunar
