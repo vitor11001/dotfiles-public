0decf0b59304b3bebf8ee16fe3d0fbaeecd87e3a
Vitor11001 <josevitorleao1@gmail.com>
2024-10-12 23:59:06 -0300

    .

--- scripts/install_bspwm.sh
+++ scripts/install_bspwm.sh
@@ -26,12 +26,12 @@ sudo pacman -Syu
 echo "Instalando pacotes necessários..."
 confirm "Você deseja instalar os pacotes do Xorg?"
 sudo pacman -S --needed \
-    xorg-server \       # O servidor X, necessário para qualquer ambiente gráfico.
-    xorg-xinit \        # Inicializador do servidor X.
-    xorg-xrandr \       # Utilitário para configurar a resolução da tela.
-    xorg-xsetroot \     # Utilitário para definir a cor da raiz.
-    xorg-xprop \        # Utilitário para visualizar propriedades de janelas.
-    xorg-xev            # Utilitário para visualizar eventos do teclado e mouse.
+    xorg-server \
+    xorg-xinit \
+    xorg-xrandr \
+    xorg-xsetroot \
+    xorg-xprop \
+    xorg-xev
 
 confirm "Você deseja instalar os drivers NVIDIA e ferramentas?"
 sudo pacman -S --needed \
@@ -68,7 +68,7 @@ sudo pacman -S --needed \
     bluez-utils \
     blueman \
     htop \
-    ntfs-3g \        # Driver NTFS
+    ntfs-3g \
     pipewire \
     pipewire-alsa \
     pipewire-pulse \
